768e92a3eb5cf860e6a86627ceed7820362a506c
privateuser123321 <>
2024-10-19 11:14:58 +0300

    Update xmlreader.py

--- xmlreader.py
+++ xmlreader.py
@@ -1,17 +1,6 @@
-import xml.etree.ElementTree as ET
-
 import xmltodict
-
-from pprint import pprint
 
 with open('alan.osrl') as fd:
     doc = xmltodict.parse(fd.read())
 
-pprint(doc)
-
-# tree = ET.parse('alan.osrl')
-# root = tree.getroot()  
-# print(root[1])
-
-# # print the attributes of the first tag  
-# print(root[0].attrib) 
+print(doc)
